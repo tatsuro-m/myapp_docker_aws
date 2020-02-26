59fdc3576dc115feb13bf00cddbdb966a73826c1
Tatsuro Miyamoto <mtatsu0130@gmail.com>
2020-02-27 06:31:41 +0900

    timeoutにならないように時間をあげました。

--- config/puma.rb
+++ config/puma.rb
@@ -8,4 +8,4 @@ app_root = File.expand_path("../..", __FILE__)
 bind "unix://#{app_root}/tmp/sockets/puma.sock"
 
 stdout_redirect "#{app_root}/log/puma.stdout.log", "#{app_root}/log/puma.stderr.log", true
-worker_timeout 60
+worker_timeout 3000
